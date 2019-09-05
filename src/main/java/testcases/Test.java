326bb423ad0872038b03339eb9cdfbb3a03021ee
KumaresanPftec <10095@CHETPDT011.PESPL.LOCAL>
2019-09-05 15:01:12 +0530

    Lease & Test

---
+++ src/main/java/testcases/Test.java
@@ -0,0 +1,34 @@
+package testcases;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s = "College";
+		
+		
+		for(int i=s.length()-1; i>=0; i--) {
+			
+			char c = s.charAt(i);
+			
+			System.out.print(c);
+			
+		}
+		
+		/*
+		 * int n=564, sum=0, r, temp
+		 *  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10; 
+		   if(temp==sum)    
+   System.out.println("s ");    
+  else    
+   System.out.println("n"); 
+		     */ 
+		  }
+
+	}
+
+
